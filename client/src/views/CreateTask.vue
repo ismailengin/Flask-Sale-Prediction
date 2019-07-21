d99af40a8498e6c67a7bf19a2c714d068bf3dea5
c0rt3x <iengintr@gmail.com>
2019-07-21 14:51:17 +0300

    createTask first template

---
+++ client/src/views/CreateTask.vue
@@ -0,0 +1,21 @@
+<template>
+ <form @submit="createTask">
+  <div class="form-group" >
+    <label for="taskName">Email address</label>
+    <input type="email" class="form-control" id="taskName" placeholder="Task Name">
+  </div>
+  <div class="form-group">
+    <label for="taskFile">Password</label>
+    <input type="file" class="form-control" id="taskFile" placeholder="Task File">
+  </div>
+  <div class="form-group">
+    <label for="predictionStep">Prediction Step</label>
+    <input type="password" class="form-control" id="predictionStep" placeholder="Prediction Step">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</template>
