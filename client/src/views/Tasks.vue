0d10f0d9cd134d7d44bc6b0438fdf26a89d2ce74
c0rt3x <iengintr@gmail.com>
2019-07-24 19:20:39 +0300

    start-endtime fields for prediction times

--- client/src/views/Tasks.vue
+++ client/src/views/Tasks.vue
@@ -10,6 +10,8 @@
           <tbody>
             <tr v-for="(tasks, index) in tasks" :key="index">
               <td>{{ tasks.taskname }}</td>
+              <td>{{startTime}}</td>
+              <td>{{endTime}}</td>
               <td>
                 <div class="btn-group" role="group">
                   <button type="button" class="btn btn-warning btn-sm"
